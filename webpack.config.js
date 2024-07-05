f52ccecf706588413e33daa339f51e485493f3a6
IcelandSpar <victorasielrodriguez@gmail.com>
2024-07-05 06:39:52 -0500

    Add general blueprint to app and search icon

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
+// eslint-disable-next-line no-undef
 const path = require('path');
+// eslint-disable-next-line no-undef
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// eslint-disable-next-line no-undef
 module.exports = {
 
   entry: './src/index.js',
   output: {
     filename: 'main.js',
+    // eslint-disable-next-line no-undef
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
