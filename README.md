4e72591580ad16f3efe811a49d56b6a588ef0e1d
IcelandSpar <victorasielrodriguez@gmail.com>
2024-07-14 00:33:57 -0500

    Add weather conditions and credit images used
    Made precipitation (rain, snow, & thunder) higher priority when choosing which theme and image to use.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # weather-app
 API practice, Weather App
+
+Credit to artists for photos used in this page: 
+
+    Image by <a href="https://pixabay.com/users/aalmeidah-4277022/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=8033597">Almeida</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=8033597">Pixabay</a>
+
+     Image by <a href="https://pixabay.com/users/alanajordan-25247407/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=8842379">Alana Jordan</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=8842379">Pixabay</a> 
+
+     Image by <a href="https://pixabay.com/users/kyra_starr-23387830/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=8205354">Kyra Starr</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=8205354">Pixabay</a> 
+
+     Image by <a href="https://pixabay.com/users/riki32-8327183/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7249528">Riki32</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7249528">Pixabay</a> 
+
+     Image by <a href="https://pixabay.com/users/aalmeidah-4277022/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7191822">Almeida</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7191822">Pixabay</a> 
+
+     Image by <a href="https://pixabay.com/users/aalmeidah-4277022/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7191822">Almeida</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7191822">Pixabay</a> 
--- src/weather-theme.js
+++ src/weather-theme.js
@@ -8,26 +8,24 @@ import snow from './snow.jpg'
 
 export async function currentWeatherTheme(condition) {
     
-    if(await condition.includes('cloudy') || await condition.includes('Overcast') || await condition.includes('Cloud')) {
-        firstCardDom.contentContainer.style.backgroundImage = 'linear-gradient(#344964, #303940)';
-        return firstCardDom.currentContent.style.backgroundImage = `url(${await cloudy})`
-
-    } else if (await condition.includes('storm') || await condition.includes('lightning') || await condition.includes('thunder')) {
+    if (await condition.includes('storm') || await condition.includes('lightning') || await condition.includes('thunder') || await condition.includes('Thundery') || await condition.includes('Thunder')) {
         firstCardDom.contentContainer.style.backgroundImage = 'linear-gradient(#2E3F59, #171D2D)';
         return firstCardDom.currentContent.style.backgroundImage = `url(${await storm})`
-    } else if (await condition.includes('sunny')) {
-        firstCardDom.contentContainer.style.backgroundImage = 'linear-gradient(#da673a, #1B1F3A)';
-        return firstCardDom.currentContent.style.backgroundImage = `url(${await sunset})`
-        
+    } else if (await condition.includes('snow') || await condition.includes('freeze') || await condition.includes('hail') || await condition.includes('freez') || await condition.includes('Freez') || await condition.includes('Ice') || await condition.includes('ice')) {
+        firstCardDom.contentContainer.style.backgroundImage = 'linear-gradient(#ABB9BC, #343440)';
+        return firstCardDom.currentContent.style.backgroundImage = `url(${await snow})`
     } else if (await condition.includes('rain') || await condition.includes('drizzle') || await condition.includes('Rain')) {
         firstCardDom.contentContainer.style.backgroundImage = 'linear-gradient(#CDDBDE, #436883)';
         return firstCardDom.currentContent.style.backgroundImage = `url(${await rain})`
+    } else if (await condition.includes('sunny')) {
+        firstCardDom.contentContainer.style.backgroundImage = 'linear-gradient(#da673a, #1B1F3A)';
+        return firstCardDom.currentContent.style.backgroundImage = `url(${await sunset})`
     } else if (await condition.includes('clear') || await condition.includes('Clear')) {
         firstCardDom.contentContainer.style.backgroundImage = 'linear-gradient(#436883, #5F666E)';
         return firstCardDom.currentContent.style.backgroundImage = `url(${await clear})`
-    } else if (await condition.includes('snow') || await condition.includes('freeze') || await condition.includes('hail')) {
-        firstCardDom.contentContainer.style.backgroundImage = 'linear-gradient(#ABB9BC, #343440)';
-        return firstCardDom.currentContent.style.backgroundImage = `url(${await snow})`
+    } else if (await condition.includes('cloudy') || await condition.includes('Overcast') || await condition.includes('overcast') || await condition.includes('Cloud') || await condition.includes('fog') || await condition.includes('Fog')) {
+        firstCardDom.contentContainer.style.backgroundImage = 'linear-gradient(#344964, #303940)';
+        return firstCardDom.currentContent.style.backgroundImage = `url(${await cloudy})`
     } else {
         firstCardDom.contentContainer.style.backgroundImage = 'linear-gradient(#da673a, #1B1F3A)';
         return firstCardDom.currentContent.style.backgroundImage = `url(${await sunset})`
